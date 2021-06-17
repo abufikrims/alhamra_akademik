76fb983e262814788bde8510c9fcf1465aa5d01a
Imam Masyhuri <abufikri.ms@gmail.com>
2021-06-17 12:00:18 +0700

    Revisi Uang Saku

--- models/uangsaku.py
+++ models/uangsaku.py
@@ -1,4 +1,4 @@
-from odoo import api, fields, models, _
+from odoo import api, fields, models, exceptions, _
 
 class uang_saku(models.Model):
     _name = 'uang.saku'
@@ -9,6 +9,9 @@ class uang_saku(models.Model):
     tgl_transaksi = fields.Date(string='Tanggal', required=True, default=fields.Date.context_today)
     no_va_saku = fields.Char(string='VA Uang Saku', related='siswa_id.va_saku')
     waktu_transaksi = fields.Char(string='Waktu Transaksi')
+    jns_transaksi = fields.Selection(string='Jenis Transaksi', selection=[('masuk', 'Uang Masuk'), ('keluar', 'Uang Keluar')], default='masuk')
+    saldo_saku = fields.Float(string='Saldo Awal U.Saku')
+     
     amount_in = fields.Float('Nominal Masuk')
     amount_out = fields.Float('Nominal Keluar')
     ref_transaksi = fields.Char('Ref Transaksi')
@@ -29,8 +32,33 @@ class uang_saku(models.Model):
 
     def action_draft(self):
         return self.write({'state': 'draft'})
+    
+    
+    @api.onchange('siswa_id')
+    def _onchange_siswa_id(self):
+        for rec in self:
+            if rec.siswa_id:
+                rec.saldo_saku = rec.siswa_id.saldo_uang_saku
+
+    @api.constrains('amount_in','amount_out','jns_transaksi','saldo_saku')
+    def _check_saldo(self):
+        # #saldo = self.env['res.partner'].search(['partner_id','=',self.siswa_id]).saldo_uang_saku
+        # context = self._context
+        # active_ids = context.get('active_ids')
+        # partner_siswa_id = self.env['res.partner'].browse(active_ids[0])
+        # saldo_saku_sekarang = partner_siswa_id.saldo_uang_saku
+
+        if self.amount_out<0 or self.amount_in<0:
+                raise exceptions.ValidationError('ERROR ! Nominal Harus Positif')
+
+        if self.jns_transaksi=='keluar':
+            if self.amount_out>self.saldo_saku:
+                raise exceptions.ValidationError('ERROR ! Saldo Pengambilan tidak bisa melebihi SALDO UANG SAKU')
+        
+
 
     @api.model
     def create(self, vals):
         vals['name'] = self.env['ir.sequence'].next_by_code('uang.saku')
+        vals['state'] = 'confirmed'
         return super(uang_saku, self).create(vals)
